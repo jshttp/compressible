5b4c0d400a7bc291e8a2f30607311862a7a9bb02
Fishrock123 <fishrock123@rocketmail.com>
2013-12-20 17:07:36 -0500

    Updated tests to work with 0559b29

--- test.js
+++ test.js
@@ -3,10 +3,10 @@ var assert = require('assert')
 var specifications = require('./specifications.json')
 var compressible = require('./')
 
-specifications.forEach(function (type) {
-  var value = type.compressible
+for (var type in specifications) {
+  var value = specifications[type].compressible
   if (typeof value !== 'boolean') return
-  it(type.mime + ' should' + (value ? ' ' : ' not ') + 'be compressible', function () {
-    assert.equal(compressible(type.mime), value)
+  it(type + ' should' + (value ? ' ' : ' not ') + 'be compressible', function () {
+    assert.equal(compressible(type), value)
   })
-})
+}
