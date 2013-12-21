50461d5e3bab3daf9e0d13f0e6a0130ac1d72f91
Fishrock123 <fishrock123@rocketmail.com>
2013-12-20 20:52:24 -0500

    Tests for 14249f2 & test descriptions.

--- test.js
+++ test.js
@@ -3,10 +3,32 @@ var assert = require('assert')
 var specifications = require('./specifications.json')
 var compressible = require('./')
 
-for (var type in specifications) {
-  var value = specifications[type].compressible
-  if (typeof value !== 'boolean') return
-  it(type + ' should' + (value ? ' ' : ' not ') + 'be compressible', function () {
-    assert.equal(compressible(type), value)
+// None of these should be actual types so that the lookup will never include them.
+var example_types = [
+  { type: 'something/text', should: true },
+  { type: 'thingie/dart', should: true },
+  { type: 'type/json', should: true },
+  { type: 'ecmascript/6', should: true },
+  { type: 'data/beans+xml', should: true },
+  { type: 'asdf/nope', should: false },
+  { type: 'cats', should: false }
+]
+
+describe('regex tests', function () {
+  example_types.forEach(function (example) {
+    it(example.type + ' should' + (example.should ? ' ' : ' not ') + 'be compressible', function (done) {
+      assert.equal(compressible(example.type), example.should)
+      done()
+    })
   })
-}
+})
+
+describe('spec lookup tests', function () {
+  for (var type in specifications) {
+    var value = specifications[type].compressible
+    if (typeof value !== 'boolean') return
+    it(type + ' should' + (value ? ' ' : ' not ') + 'be compressible', function () {
+      assert.equal(compressible(type), value)
+    })
+  }
+})
